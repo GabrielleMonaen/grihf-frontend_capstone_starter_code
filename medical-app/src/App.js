0834740e5cd651db3b259c7a1a1e846bb2db18c4
GabrielleMonaen <gabrielle.monaen@protonmail.com>
2024-02-21 19:47:03 +0000

    Added React components Navbar and LandingPage

--- medical-app/src/App.js
+++ medical-app/src/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import BookingConsultation from './Components/BookingConsultation';
+import Navbar from './Components/Navbar/Navbar';
+import LandingPage from './Components/LandingPage/LandingPage';
 
 function App() {
   return (
-    <Router>
+    <div className="App">
+    <BrowserRouter>
+      <Navbar />
       <Routes>
+        <Route path="/" element={<LandingPage />} />
         <Route path="/booking-consultation" element={<BookingConsultation />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
+    </div>
   );
 }
 
