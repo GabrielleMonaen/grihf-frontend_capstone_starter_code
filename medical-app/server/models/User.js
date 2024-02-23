b91b5f600964e68fb2b6344857cf720fcd8eb1d2
GabrielleMonaen <gabrielle.monaen@protonmail.com>
2024-02-23 16:19:11 +0000

    Added the Setauthtoken.js file and the server folder

--- medical-app/server/models/User.js
+++ medical-app/server/models/User.js
@@ -1,27 +1,23 @@
-// models/User.js
 const mongoose = require('mongoose');
+const { Schema } = require('mongoose');
 
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  phone: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-});
+const userSchema = new Schema({
+    email: { 
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    name: {
+        type: String,
+        required: true,
+    },
+    phone:{
+        type: String,
+        required: true,
+    }
+}, {timestamps: true});
 
-const User = mongoose.model('User', userSchema);
-
-module.exports = User;
+module.exports = mongoose.model('user', userSchema);
--- medical-app/src/config.js
+++ medical-app/src/config.js
@@ -1,3 +1,5 @@
-const API_URL = 'http://localhost:5000';
-
-export { API_URL };
+export const API_URL = window.location.hostname === "localhost" ? "<add your theia server side url>" : "add your theia server side url";
+console.log(
+    "API_URL :",
+    API_URL
+);
